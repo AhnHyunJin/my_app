8053c14d4a7042579cc6ed6b49f745c0c2d84ebb
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-06-02 15:26:09 +0900

    Updates

---
+++ precache-manifest.e920625c30d4ee7d2a3e215cae489fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff77e674c1244b4aaf42c0dd7fec6ba6",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "093ee3cff052b9adfd8e",
+    "url": "/my_app/static/css/main.3b7fe94c.chunk.css"
+  },
+  {
+    "revision": "7bb205c1888dc339a680",
+    "url": "/my_app/static/js/2.d15e1bf7.chunk.js"
+  },
+  {
+    "revision": "093ee3cff052b9adfd8e",
+    "url": "/my_app/static/js/main.4f76d235.chunk.js"
+  },
+  {
+    "revision": "25c9988c66468b3bdff7",
+    "url": "/my_app/static/js/runtime~main.06f84023.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
